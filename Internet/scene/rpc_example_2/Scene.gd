c83946ffb17427b00208c06391212d242c04769d
thiagola92 <thiagola92@gmail.com>
2021-08-04 08:20:49 -0300

    Add option to choose IP

--- Internet/scene/rpc_example_2/Scene.gd
+++ Internet/scene/rpc_example_2/Scene.gd
@@ -2,7 +2,6 @@ extends Control
 
 const Player = preload("res://scene/rpc_example_2/Player.tscn")
 
-var ip = "127.0.0.1"
 var port = 1992
 var peer = NetworkedMultiplayerENet.new()
 
@@ -11,16 +10,18 @@ var players = {}
 
 func _ready():
 	get_tree().connect("network_peer_connected", self, "_add_child")
-	_make_connection()
-	_add_child(
-		get_tree().get_network_unique_id()
-	)
 
 
 func _make_connection():
+	var ip = $Left/IP.text
+	
 	if peer.create_server(port) != OK:
 		peer.create_client(ip, port)
+	
 	get_tree().network_peer = peer
+	_add_child(
+		get_tree().get_network_unique_id()
+	)
 
 
 func _add_child(id : int):
